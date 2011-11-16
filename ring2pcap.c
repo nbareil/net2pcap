709b11ba9712368ea147d1a1e7c573fc4c7d407f
Nicolas Bareil <nico@chdir.org>
2011-11-16 10:36:20 +0100

    ca marche

--- ring2pcap.c
+++ ring2pcap.c
@@ -8,9 +8,11 @@
 #include <unistd.h>
 #include <signal.h>
 #include <fcntl.h>
+#include <poll.h>
 #include <sys/uio.h>
 #include <sys/time.h>
 #include <sys/ioctl.h>
+#include <sys/select.h>
 #include <arpa/inet.h>
 #include <sys/mman.h>
 #include <sys/types.h>
@@ -57,10 +59,41 @@ struct pcap_pkthdr {
 
 #define SNAPLEN 65536
 
-#define TP_BLOCK_NR  4          /* greater than TPACKET_HDRLEN */
-#define TP_FRAMES_NR 8          /* exactly frames_per_block*tp_block_nr */
-#define TP_FRAME_SIZE    2048   /* multiple of TPACKET_ALIGNMENT */
-#define TP_BLOCK_MINSIZE 4096   /* multiple of PAGE_SIZE */
+/*
+  Need to *NOT* verify the following constraints:
+
+  tp_block_size <= 0
+  tp_block_size & (PAGE_SIZE - 1)
+  tp_frame_size < po->tp_hdrlen + po->tp_reserve
+  tp_frame_size & (TPACKET_ALIGNMENT - 1)
+  tp_block_size/tp_frame_size <= 0
+  (tp_block_size/tp_frame_size * tp_block_nr) != tp_frame_nr
+
+  source: net/packet/af_packet.c:2500
+*/
+
+#define PAGE_SIZE 4096 /* XXX */
+
+/* #define TP_BLOCK_NR  40 */
+/* #define TP_FRAME_SIZE TPACKET_ALIGN(1600) */
+/* #define TP_BLOCK_SIZE PAGE_SIZE */
+/* #define TP_FRAMES_NR (TP_BLOCK_NR*(TP_BLOCK_SIZE/TP_FRAME_SIZE)) */
+
+/* #define TP_BLOCK_NR  (256/4) */
+/* #define TP_BLOCK_SIZE (4096 << 2) */
+/* #define TP_FRAME_SIZE 1600 */
+/* #define TP_FRAMES_NR (TP_BLOCK_NR*(TP_BLOCK_SIZE/TP_FRAME_SIZE)) */
+
+/* #define TP_BLOCK_NR  64 */
+/* #define TP_BLOCK_SIZE 4096 FONCTIONNEL */ 
+/* #define TP_FRAME_SIZE 1024 */
+/* #define TP_FRAMES_NR 4*64 */
+
+#define FRAMES_PER_BLOCK 8
+#define TP_BLOCK_NR  8
+#define TP_FRAME_SIZE 65536
+#define TP_BLOCK_SIZE TP_FRAME_SIZE*FRAMES_PER_BLOCK
+#define TP_FRAMES_NR TP_BLOCK_NR*FRAMES_PER_BLOCK
 
 #define TPKT2LOAD(x) ((char *) x + TPACKET_HDRLEN + 14)
 #define LOAD2TPKT(x) ((char *) x - TPACKET_HDRLEN - 14)
@@ -68,7 +101,7 @@ struct pcap_pkthdr {
 #define MAX_LEN_ERRORMSG 2048
 #define ERROR(x...) do{LOG(LOG_ERR, "ERROR: " x);exit(EXIT_FAILURE);}while(0)
 #define LOG(prio,x...) do{if(daemonize > 1) syslog(prio, x);    \
-          else fprintf(stderr,"net2pcap: " x);} while(0)
+          else fprintf(stderr,"ring2pcap: " x);} while(0)
 
 int daemonize = 0;
 volatile sig_atomic_t term_received = 0;
@@ -138,9 +171,10 @@ void *setup_packetring(int sock)
      int ret, timesource = SOF_TIMESTAMPING_RAW_HARDWARE ; /* XXX or SOF_TIMESTAMPING_SYS_HARDWARE */
      size_t ringsize;
      void *ringbuf;
+     struct sockaddr_ll addr;
      struct tpacket_req treq =
           {
-               .tp_block_size = TP_BLOCK_MINSIZE,
+               .tp_block_size = TP_BLOCK_SIZE,
                .tp_block_nr   = TP_BLOCK_NR,
                .tp_frame_size = TP_FRAME_SIZE,
                .tp_frame_nr   = TP_FRAMES_NR,
@@ -162,6 +196,18 @@ void *setup_packetring(int sock)
      if (ringbuf == MAP_FAILED)
           PERROR("mmap(PACKET_RX_RING)");
 
+	
+     /* bind the packet socket: needed to poll() */
+     memset(&addr, 0, sizeof(addr));
+     addr.sll_family   = AF_PACKET;
+     addr.sll_protocol = htons(0x03);
+     addr.sll_ifindex  = 0;
+     addr.sll_hatype   = 0;
+     addr.sll_pkttype  = 0;
+     addr.sll_halen    = 0;
+     if ( bind(sock, (struct sockaddr *)&addr, sizeof(addr)))
+             PERROR("bind()");
+
      return ringbuf;
 }
 
@@ -169,19 +215,21 @@ void flush_packets(int fd, struct iovec *iov, unsigned long n)
 {
      int i;
 
-     if (vmsplice(fd, iov, n, SPLICE_F_MOVE) == -1)
-          PERROR("vmsplice()");
+     /* if (vmsplice(fd, iov, n, 0) == -1) */
+     /*      PERROR("vmsplice()"); */
 
      /* packets were written, we can now release the ring entries */
      for (i=1 ; i < n; i += 2)
      {
           struct iovec *v = (iov+i);
           struct tpacket_hdr *tpkt = (struct tpacket_hdr *) LOAD2TPKT(v->iov_base);
+          printf("  - %p\n", tpkt);
           tpkt->tp_status = TP_STATUS_KERNEL;
+
      }
 }
 
-void packet_harvester(int fd, void *base)
+void packet_harvester(int sock, int fd, void *base)
 {
      unsigned int i, j, k, pktcount;
      struct pcap_pkthdr pcaphdr[2*TP_FRAMES_NR];
@@ -191,62 +239,86 @@ void packet_harvester(int fd, void *base)
      i = j = k = pktcount = 0;
      while (! term_received)
      {
-          struct tpacket_hdr *hdr;
-          struct pcap_pkthdr *phdr;
+             struct tpacket_hdr *hdr;
 
-          hdr  = (struct tpacket_hdr *) ((char *)base + i*TP_FRAME_SIZE);
-          
-          if (hdr->tp_status & TP_STATUS_USER)
-          {
-               if (hdr->tp_status & TP_STATUS_LOSING)
-               {
-                    printf("loosing packets, fuck!\n");
-                    exit(0);
-               }
+             hdr  = (struct tpacket_hdr *) ((char *)base + i*TP_FRAME_SIZE);
 
-               phdr = (pcaphdr + k++);
+             while (! hdr->tp_status) 
+             {
+                     struct pollfd pfd;
 
-               phdr->len        = hdr->tp_len;
-               phdr->caplen     = hdr->tp_snaplen;
-               phdr->ts.tv_sec  = NATIVE2COMPAT(hdr->tp_sec);
-               phdr->ts.tv_usec = NATIVE2COMPAT(hdr->tp_usec);
+                     pfd.fd      = sock;
+                     pfd.revents = 0;
+                     pfd.events  = POLLIN|POLLRDNORM|POLLERR;
 
-               /* printf("%x %x %x %x\n", phdr->len, phdr->caplen, phdr->ts.tv_sec, phdr->ts.tv_usec); */
-               iov[j].iov_len = sizeof(pcaphdr);
-               iov[j].iov_base = phdr;
-               j++;
+                     poll(&pfd, 1, -1);
+             }
+             if (hdr->tp_status & TP_STATUS_LOSING)
+                     ERROR("loosing packets, fuck!\n");
 
-               iov[j].iov_len = hdr->tp_len;
-               iov[j].iov_base = TPKT2LOAD(hdr);
-               /* printf("mac=%d net=%d hdr=%p load=%p\n", hdr->tp_mac, hdr->tp_net, hdr, iov[j].iov_base); */
+             hdr->tp_status = 0;
 
-               hexdump(iov[j].iov_base, iov[j].iov_len);
-               j++;
-               pktcount++;
-
-               if (pktcount > FLUSH_PACKETS)
-               {
-                       *flushpkt = 1;
-                       if (futex(flushpkt, FUTEX_WAKE, 1, NULL, NULL, 0) != 1)
-                       {
-                               perror("futex_wake()");
-                               break;
-                       }
-               }
-          }
-          /* XXX check that 2*TP_FRAMES_NR < IOV_MAX */
-          i++;
-          if (i >= TP_FRAMES_NR)
-          {
-               /* if (j > FLUSH_THRESHOLD) */
-               if (j > 0)
-               {
-                    flush_packets(fd, iov, j);
-                    j = k = 0;
-               }
-               i = 0;
-          }
+             i++;
+             if (i >= TP_FRAMES_NR)
+                     i = 0;
      }
+             /* for (i=0 ; i < TP_FRAMES_NR ; i++) */
+             /* { */
+             /*         /\* XXX check that 2*TP_FRAMES_NR < IOV_MAX *\/ */
+             /*         struct tpacket_hdr *hdr; */
+             /*         struct pcap_pkthdr *phdr; */
+
+             /*         hdr  = (struct tpacket_hdr *) ((char *)base + i*TP_FRAME_SIZE); */
+          
+             /*         /\* if (hdr->tp_status == TP_STATUS_KERNEL) *\/ */
+
+
+             /*         if (hdr->tp_status & TP_STATUS_USER) */
+             /*         { */
+             /*                 printf("  + %p\n", hdr); */
+             /*                 if (hdr->tp_status & TP_STATUS_LOSING) */
+             /*                         ERROR("loosing packets, fuck!\n"); */
+                                     
+             /*                 phdr = (pcaphdr + k++); */
+
+             /*                 phdr->len        = hdr->tp_len; */
+             /*                 phdr->caplen     = hdr->tp_snaplen; */
+             /*                 phdr->ts.tv_sec  = NATIVE2COMPAT(hdr->tp_sec); */
+             /*                 phdr->ts.tv_usec = NATIVE2COMPAT(hdr->tp_usec); */
+
+             /*                 /\* printf("%x %x %x %x\n", phdr->len, phdr->caplen, phdr->ts.tv_sec, phdr->ts.tv_usec); *\/ */
+             /*                 iov[j].iov_len = sizeof(pcaphdr); */
+             /*                 iov[j].iov_base = phdr; */
+             /*                 j++; */
+
+             /*                 iov[j].iov_len = hdr->tp_len; */
+             /*                 iov[j].iov_base = TPKT2LOAD(hdr); */
+             /*                 /\* printf("mac=%d net=%d hdr=%p load=%p\n", hdr->tp_mac, hdr->tp_net, hdr, iov[j].iov_base); *\/ */
+
+             /*                 //hexdump(iov[j].iov_base, iov[j].iov_len); */
+             /*                 j++; */
+             /*                 pktcount++; */
+             /*         } */
+             /* } */
+
+
+             /* if (j > 0) */
+             /* { */
+             /*         flush_packets(fd, iov, j); */
+             /*         j = k = i = 0; */
+             /* } */
+
+          /* if (i >= TP_FRAMES_NR) */
+          /* { */
+          /*      /\* if (j > FLUSH_THRESHOLD) *\/ */
+          /*      if (j > 0) */
+          /*      { */
+          /*           flush_packets(fd, iov, j); */
+          /*           j = k = 0; */
+          /*      } */
+          /*      i = 0; */
+          /* } */
+     /* } */
 }
 
 void writer(char *filename, int fd_in)
@@ -293,11 +365,16 @@ void writer(char *filename, int fd_in)
 
      while (1)
      {
-             if (futex(processme, FUTEX_WAIT, 1, NULL, NULL, 0) != 0)
-             {
-                     PERROR("futex_wait()");
-                     break;
-             }
+             fd_set set;
+
+             FD_ZERO(&set);
+             FD_SET(fd_in, &set);
+
+             if (select(fd_in+1, &set, NULL, NULL, NULL) == -1)
+                     PERROR("select()");
+
+             if (! FD_ISSET(fd_in, &set))
+                     continue;
 
              len = splice(fd_in, NULL, fd_out, NULL, 4096, SPLICE_F_MOVE|SPLICE_F_MORE);
              if (len == -1)
@@ -351,7 +428,7 @@ int main(int argc, char *argv[])
      ifdx = get_iface_index(sock, iface);
      set_promiscuous(sock, ifdx, 1);
      ringbuf = setup_packetring(sock);
-     packet_harvester(pipes[1], ringbuf);
+     packet_harvester(sock, pipes[1], ringbuf);
      set_promiscuous(sock, ifdx, 0);
 
      /* XXX: kill shared memory? */
